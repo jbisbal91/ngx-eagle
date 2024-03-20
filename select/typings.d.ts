f81c104d5ab2ce8daec5e01ea41044f3f287d4a1
jbisbal91 <jbisbal91@gmail.com>
2024-03-19 17:14:51 -0700

    updated lib

--- select/typings.d.ts
+++
@@ -1,7 +0,0 @@
-declare const FormFieldSize: readonly ["small", "medium", "large"];
-export type NgxSize = (typeof FormFieldSize)[number];
-declare const FormFieldRounded: readonly ["small", "medium", "large"];
-export type NgxRounded = (typeof FormFieldRounded)[number];
-export declare const FormFieldFillMode: readonly ["filled", "outlined"];
-export type NgxFillMode = (typeof FormFieldFillMode)[number];
-export {};
